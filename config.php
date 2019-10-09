736f35784b3ae9801f202dcc7b56538b93ea1c16
Kun Su <kun@Kuns-Macbook-Pro.local>
2019-10-09 14:08:19 -0700

    Fix phpmyadmin problem

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     server with default setting (user 'root' with no password) */
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'kunsu');
-    define('DB_PASSWORD', '');
+    define('DB_PASSWORD', '123456');
     define('DB_NAME', 'kunsu');
     
     /* Attempt to connect to MySQL database */
