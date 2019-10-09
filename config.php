b97128308b0d00c4638527f065e09a33b0d1b85c
Kun Su <kun@Kuns-Macbook-Pro.local>
2019-10-08 23:53:40 -0700

    Lab: Secure Section

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
     /* Database credentials. Assuming you are running MySQL
     server with default setting (user 'root' with no password) */
     define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '123qweasdzxC!');
+    define('DB_USERNAME', 'kunsu');
+    define('DB_PASSWORD', '123456');
     define('DB_NAME', 'kunsu');
     
     /* Attempt to connect to MySQL database */
