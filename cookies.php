fd9a28419209a1ea422d1c179b69426df1c670ab
Kun Su <kun@Kuns-Macbook-Pro.local>
2019-11-05 16:17:14 -0800

    Fix Bug

--- cookies.php
+++ cookies.php
@@ -29,8 +29,8 @@ function get_cookies() {
         $pages[$page_cached_time] = $page_name;
     }
     krsort($pages);
-    $url = str_replace(' ', '%20', $page_name);
     foreach ($pages as $page_cached_time => $page_name) {
+        $url = str_replace(' ', '%20', $page_name);
         echo "<br><a href=" . $url . ".php>" . $page_name . "</a><br>";
     }
     echo "</aside>";
